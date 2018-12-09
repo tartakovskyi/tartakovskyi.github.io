fc73a903aed1c14121ebb0d07af495d137063d6a
tartakovskyi <vvt2001@ukr.net>
2018-12-09 21:30:48 +0200

    l19 js-dom complete

--- lesson_19_js_dom/src/js/dom.js
+++ lesson_19_js_dom/src/js/dom.js
@@ -65,14 +65,13 @@ for (let i=0; i<playList.length; i++) {
     baTune.classList.add('ba-tune');
     baPlaylistList.appendChild(baTune);
 
-   /* const baTuneLike = document.createElement('svg');
+/*    const baTuneLike = document.createElement('svg');
     baTuneLike.setAttribute('width', '10');
     baTuneLike.setAttribute('height', '10');
     baTuneLike.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    baTuneLike.setAttribute('viewBox', '"0 0 32 32');
-    baTuneLike.classList.add('ba-tune__like');
-    baTuneLike.innerHTML = '<path fill="currentcolor" d="M29.193 5.265c-3.629-3.596-9.432-3.671-13.191-0.288-3.76-3.383-9.561-3.308-13.192 0.288-3.741 3.704-3.741 9.709 0 13.415 1.069 1.059 11.053 10.941 11.053 10.941 1.183 1.172 3.096 1.172 4.278 0 0 0 10.932-10.822 11.053-10.941 3.742-3.706 3.742-9.711-0.001-13.415zM27.768 17.268l-11.053 10.941c-0.393 0.391-1.034 0.391-1.425 0l-11.053-10.941c-2.95-2.92-2.95-7.671 0-10.591 2.844-2.815 7.416-2.914 10.409-0.222l1.356 1.22 1.355-1.22c2.994-2.692 7.566-2.594 10.41 0.222 2.95 2.919 2.95 7.67 0.001 10.591zM9.253 7.501c0.277 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5h-0.001c-1.794 0-3.249 1.455-3.249 3.249v0.001c0 0.276-0.224 0.5-0.5 0.5s-0.5-0.224-0.5-0.5v0c0-2.346 1.901-4.247 4.246-4.249 0.002 0 0.002-0.001 0.004-0.001z"></path>'
-    baTune.appendChild(baTuneLike);*/
+    baTuneLike.setAttribute('viewBox', '0 0 32 32');
+    baTuneLike.classList.add('ba-tune__like');*/
+    /*   baTuneLike.innerHTML = '<path fill="currentcolor" d="M29.193 5.265c-3.629-3.596-9.432-3.671-13.191-0.288-3.76-3.383-9.561-3.308-13.192 0.288-3.741 3.704-3.741 9.709 0 13.415 1.069 1.059 11.053 10.941 11.053 10.941 1.183 1.172 3.096 1.172 4.278 0 0 0 10.932-10.822 11.053-10.941 3.742-3.706 3.742-9.711-0.001-13.415zM27.768 17.268l-11.053 10.941c-0.393 0.391-1.034 0.391-1.425 0l-11.053-10.941c-2.95-2.92-2.95-7.671 0-10.591 2.844-2.815 7.416-2.914 10.409-0.222l1.356 1.22 1.355-1.22c2.994-2.692 7.566-2.594 10.41 0.222 2.95 2.919 2.95 7.67 0.001 10.591zM9.253 7.501c0.277 0 0.5 0.224 0.5 0.5s-0.224 0.5-0.5 0.5h-0.001c-1.794 0-3.249 1.455-3.249 3.249v0.001c0 0.276-0.224 0.5-0.5 0.5s-0.5-0.224-0.5-0.5v0c0-2.346 1.901-4.247 4.246-4.249 0.002 0 0.002-0.001 0.004-0.001z"></path>'*/
     const baTuneLike = document.createElement('img');
     baTuneLike.setAttribute('src', 'img/icon-heart.svg');
     baTuneLike.classList.add('ba-tune__like');
@@ -82,8 +81,8 @@ for (let i=0; i<playList.length; i++) {
 
     /*const baTuneLikeUse = document.createElement('use');
     baTuneLikeUse.setAttribute('xlink:href', 'img/icon-heart.svg#icon-heart');
-    baTuneLike.appendChild(baTuneLikeUse);
-    */
+    baTuneLike.appendChild(baTuneLikeUse);*/
+    
     const baTuneTop = document.createElement('div');
     baTuneTop.classList.add('ba-tune__top');
     baTune.appendChild(baTuneTop);
