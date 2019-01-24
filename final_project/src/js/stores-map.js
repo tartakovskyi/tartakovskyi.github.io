00bbbca15f8a31703e347b4d088f750806c71240
tartakovskyi <vvt2001@ukr.net>
2019-01-25 01:35:15 +0200

    final project stores

--- final_project/src/js/stores-map.js
+++ final_project/src/js/stores-map.js
@@ -5,11 +5,95 @@ var ny = {lat: 40.7683004, lng: -73.9844432};
 function initMap() {
 	var map = new google.maps.Map(document.getElementById('map'), {
 		center: london,
-		zoom: 17
+		zoom: 17,
+		disableDefaultUI: true,
+		styles: [
+		{
+			"featureType": "administrative",
+			"elementType": "labels.text.fill",
+			"stylers": [
+			{
+				"color": "#444444"
+			}
+			]
+		},
+		{
+			"featureType": "landscape",
+			"elementType": "all",
+			"stylers": [
+			{
+				"color": "#f2f2f2"
+			}
+			]
+		},
+		{
+			"featureType": "poi",
+			"elementType": "all",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "road",
+			"elementType": "all",
+			"stylers": [
+			{
+				"saturation": -100
+			},
+			{
+				"lightness": 45
+			}
+			]
+		},
+		{
+			"featureType": "road.highway",
+			"elementType": "all",
+			"stylers": [
+			{
+				"visibility": "simplified"
+			}
+			]
+		},
+		{
+			"featureType": "road.arterial",
+			"elementType": "labels.icon",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "transit",
+			"elementType": "all",
+			"stylers": [
+			{
+				"visibility": "off"
+			}
+			]
+		},
+		{
+			"featureType": "water",
+			"elementType": "all",
+			"stylers": [
+			{
+				"color": "#46bcec"
+			},
+			{
+				"visibility": "on"
+			}
+			]
+		}
+		]
 	});
-	var marker1 = new google.maps.Marker({position: london, map: map});
-	var marker2 = new google.maps.Marker({position: paris, map: map});
-	var marker3 = new google.maps.Marker({position: ny, map: map});
+
+	var markerImg = '../img/map-marker.svg'
+
+	var marker1 = new google.maps.Marker({position: london, map: map, icon: markerImg});
+	var marker2 = new google.maps.Marker({position: paris, map: map, icon: markerImg});
+	var marker3 = new google.maps.Marker({position: ny, map: map, icon: markerImg});
 
 	$('[href="#paris"]').on('click', function() {
 		map.setCenter(paris);
@@ -26,17 +110,20 @@ function initMap() {
 };
 
 ///Local Stores Tabs///
-$('#myTab [href="#paris"]').on('click', function (e) {
-  e.preventDefault();
-  $(this).tab('show');
+$('#storesTab [href="#paris"]').on('click', function (e) {
+	e.preventDefault();
+	$('tab-pane.active.show').tab('hidden')
+	$(this).tab('show');
 });
-$('#myTab [href="#london"]').on('click', function (e) {
-  e.preventDefault();
-  $(this).tab('show');
+$('#storesTab [href="#london"]').on('click', function (e) {
+	e.preventDefault();
+	$(this).tab('show');
+	$(!this).tab('dispose');
 });
-$('#myTab [href="#ny"]').on('click', function (e) {
-  e.preventDefault();
-  $(this).tab('show');
+$('#storesTab [href="#ny"]').on('click', function (e) {
+	e.preventDefault();
+	$(this).tab('show');
+	$(!this).tab('dispose');
 });
 
 
