f89bc702711b55e503b41257c50a126fbb7172f7
tartakovskyi <vvt2001@ukr.net>
2019-01-27 23:07:25 +0200

    js test objects

---
+++ js-test/src/js/prod-base.js
@@ -0,0 +1,18 @@
+const prodBase = {
+	101 : {
+		name: 'T-shirt',
+		price: '89.99',
+		description: 'The best t-shirt in the world',
+		img: 'prod-6.jpg'
+	},
+
+	102 : {
+		name: 'coat',
+		price: '205.99',
+		description: 'Very good coat',
+		img: 'prod-3.jpg'
+	}
+
+}
+
+console.log(prodBase['102']['name']);
