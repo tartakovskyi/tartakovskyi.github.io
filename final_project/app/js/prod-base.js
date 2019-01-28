638f98affa71028c29abc30fc4e31e13b75c1666
tartakovskyi <vvt2001@ukr.net>
2019-01-28 23:33:41 +0200

    js-grid

---
+++ final_project/app/js/prod-base.js
@@ -0,0 +1,32 @@
+const prodBase = {
+	101 : {
+		name: 'T-shirt ultra',
+		price: '89.99',
+		description: 'The best t-shirt in the world',
+		img: 'prod-6.jpg'
+	},
+
+	102 : {
+		name: 'coat magnum',
+		price: '205.99',
+		description: 'Very good coat',
+		img: 'prod-3.jpg'
+	},
+
+	103 : {
+		name: 'womens orange casual',
+		price: '174.99',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, ex!',
+		img: 'prod-4.jpg'
+	},
+
+	104 : {
+		name: 'mens cyan t-shirt',
+		price: '174.99',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, ex!',
+		img: 'prod-1.jpg'
+	}
+
+}
+
+$('#special-offers .offers-item-wrapper').clone().appendTo('#special-offers');
