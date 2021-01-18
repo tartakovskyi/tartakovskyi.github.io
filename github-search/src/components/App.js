affeb383efaab5d4ca5ba2ad358014454ff84f78
tartakovskyi <vvt2001@ukr.net>
2021-01-18 23:03:18 +0200

    GitHub Search created

---
+++ github-search/src/components/App.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+
+function App() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div className="container">
+          <a aria-current="page" className="navbar-brand active" href="/">Github Search Service</a>
+        </div>
+      </nav>
+      <main>
+        <div className="container text-center">
+          <h1>Github Search Service</h1>
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App;
